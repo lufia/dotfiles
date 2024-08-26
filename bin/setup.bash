756037187a58a02ead7f2ca3f75ff515d32d3c99
lufia <lufia@lufia.org>
2024-08-26 13:26:14 +0900

    stop tools.go; use runtime/debug.BuildInfo in installed binaries

--- bin/setup.bash
+++ bin/setup.bash
@@ -1,36 +1,39 @@
 #!/usr/bin/env bash
 # go-install: installs commands for Go
 
-# golang.org/x/tools
-go install golang.org/x/perf/cmd/benchstat
+# golang.org/x
+go install golang.org/x/perf/cmd/benchstat@latest
 go install golang.org/x/pkgsite/cmd/pkgsite@latest
-go install golang.org/x/tools/cmd/goimports
-go get -u golang.org/x/tools/cmd/guru
-go install golang.org/x/tools/cmd/present
-go install golang.org/x/tools/cmd/stringer
-go get -u golang.org/x/tools/go/analysis/passes/shadow/cmd/shadow
-go install golang.org/x/tools/gopls
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/tools/cmd/gonew@latest
+go install golang.org/x/tools/cmd/goyacc@latest
+go install golang.org/x/tools/cmd/present@latest
+go install golang.org/x/tools/cmd/stringer@latest
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/vuln/cmd/govulncheck@latest
 
 # github.com/golang
-go install github.com/golang/protobuf/protoc-gen-go
+go install github.com/golang/protobuf/protoc-gen-go@latest
 
 # contributing to go
-go install golang.org/x/tools/cmd/go-contrib-init
-go install golang.org/x/review/git-codereview
+go install golang.org/x/tools/cmd/go-contrib-init@latest
+go install golang.org/x/review/git-codereview@latest
 
 # optional
-go get -u github.com/davidrjenni/A
-go install rsc.io/2fa
+go install rsc.io/2fa@latest
 go install rsc.io/todo
 go get -u rsc.io/github/issue
-go install 9fans.net/go/acme/editinacme
-go install robpike.io/ivy
+go install 9fans.net/go/acme/editinacme@latest
+go install robpike.io/ivy@latest
 go install rsc.io/grepdiff
 
-#go install github.com/cli/cli/cmd/gh
-#go install github.com/hashicorp/terraform
-go install github.com/go-delve/delve/cmd/dlv
-go install github.com/nektos/act
-go install github.com/golangci/golangci-lint/cmd/golangci-lint
-go install github.com/x-motemen/blogsync
-go install github.com/google/ko
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install github.com/x-motemen/blogsync@latest
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
+
+# use pacman instead
+#go install github.com/cli/cli/cmd/gh -> github-cli
+#go install github.com/hashicorp/terraform -> terraform
+#go install github.com/nektos/act -> act
+#go install github.com/google/ko -> ko
+#go install github.com/go-delve/delve/cmd/dlv -> delve
